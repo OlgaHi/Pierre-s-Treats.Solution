3da78fe58de78801c26ceb3a15b7c1f92cf3b616
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-26 15:29:54 -0700

    add login GET/POST requests and corresponding views

--- Product/Controllers/AccountController.cs
+++ Product/Controllers/AccountController.cs
@@ -25,23 +25,42 @@ namespace Product.Controllers
     }
 
     public IActionResult Register()
-        {
-            return View();
-        }
+    {
+      return View();
+    }
 
-        [HttpPost]
-        public async Task<ActionResult> Register (RegisterViewModel model)
-        {
-            var user = new ApplicationUser { UserName = model.Email };
-            IdentityResult result = await _userManager.CreateAsync(user, model.Password); 
-            if (result.Succeeded)
-            {
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View(); 
-            }
-        }
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password); 
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View(); 
+      }
+    }
+
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
