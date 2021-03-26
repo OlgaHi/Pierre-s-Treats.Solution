156aee0200768ae7c24c21ee5571b005c149e165
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-26 11:37:15 -0700

    create flavor model

---
+++ Product/Models/Flavor.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Product.Models
+{
+  public class Flavor
+  {
+    public Flavor()
+    {
+      this.Treats = new HashSet<FlavorTreat>(); 
+    }
+
+    public int FlavorId { get; set; }
+    public string FlavorType { get; set; }
+    public virtual ICollection<FlavorTreat> Treats { get; set; }
+  }
+}
