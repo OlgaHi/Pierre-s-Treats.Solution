d15d90c0476f59eb086ef483e20cfdc457946d0f
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-27 13:15:46 -0700

    add GET/POST create treat requests and corresponding views

--- Product/Controllers/TreatsController.cs
+++ Product/Controllers/TreatsController.cs
@@ -28,5 +28,27 @@ namespace Product.Controllers
       var treatsList = _db.Treats.ToList();
       return View(treatsList);
     }
+
+    [Authorize]
+    public ActionResult Create()
+    {
+    ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorType");
+    return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat, int FlavorId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
