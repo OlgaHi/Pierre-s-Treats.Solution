b10829c10768ac764b5a03ceef94617615c58580
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-27 20:34:36 -0700

    add delete treat request and corresponding view

--- Product/Controllers/TreatsController.cs
+++ Product/Controllers/TreatsController.cs
@@ -85,5 +85,21 @@ namespace Product.Controllers
       return RedirectToAction("Index");
     }
 
+    [Authorize]
+    public ActionResult Delete(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
