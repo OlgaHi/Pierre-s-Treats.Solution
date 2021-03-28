13ca30e18d86d60cc6bbb5e89883ada7c0a09be1
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-28 16:35:34 -0700

    add GET/POST edit flavor and corresponding views

--- Product/Controllers/FlavorsController.cs
+++ Product/Controllers/FlavorsController.cs
@@ -58,6 +58,31 @@ namespace Product.Controllers
       return View(thisFlavor);
     }
 
+    [Authorize]
+    public ActionResult Edit(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "ProductName") ;
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor, int TreatId)
+    {
+      if (TreatId != 0)
+      {
+        var joinTable = _db.FlavorTreat
+        .Any(join => join.FlavorId == flavor.FlavorId && join.TreatId == TreatId);
+        if (!joinTable)
+          {
+            _db.FlavorTreat.Add(new FlavorTreat() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+          }
+      }
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
     
 
 
