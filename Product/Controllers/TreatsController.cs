ff52601f5ffe5ccc43f8e81ea27e099765d74981
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-27 20:50:53 -0700

    add GET/POST addflavor treat and corresponding views

--- Product/Controllers/TreatsController.cs
+++ Product/Controllers/TreatsController.cs
@@ -101,5 +101,29 @@ namespace Product.Controllers
       return RedirectToAction("Index");
     }
 
+    [Authorize]
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorType");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        var joinTable = _db.FlavorTreat
+        .Any(join => join.TreatId == treat.TreatId && join.FlavorId == FlavorId);
+        if (!joinTable)
+          {
+            _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+          }
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
