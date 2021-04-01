c1650088eb32c368ae8d4dab72cc7442fc2a05bc
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-01 13:42:28 -0700

    add corrections to views and Readme

--- Product/Controllers/TreatsController.cs
+++ Product/Controllers/TreatsController.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Product.Models;
-using System.Collections.Generic; 
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering; 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity; 
-using System.Threading.Tasks; 
-using System.Security.Claims;
+
 
 namespace Product.Controllers
 {
